102a70d02c4cb6f66b1f119c9050ed80ead856b7
René Preuß <rene@preuss.io>
2021-04-03 23:43:09 +0200

    Initial release candidate

--- resources/js/views/Config.vue
+++ resources/js/views/Config.vue
@@ -1,5 +1,24 @@
 <template>
-    <p>This is the config page!</p>
+    <div class="vh-100 d-flex justify-content-center">
+      <div class="col-8 align-self-center text-center">
+        <img src="https://cdn.jukebox.gg/default.jpg" class="mb-3 rounded" height="75" alt="cover">
+        <h3>Welcome to Jukebox!</h3>
+        <h5 class="text-white-50">
+          The Music Player for Streamers & Gamers
+        </h5>
+
+        <p class="py-5 text-white-50">
+          Jukebox is a music streaming platform, created by Streamers & 3rd Party Twitch Devs for streamers. Together with our artist partners we want to offer a DMCA free solution to streamers that does not mute VODs or clips or remove their music from them.
+        </p>
+
+        <a href="https://play.jukebox.gg/" class="btn btn-lg btn-primary mr-4" target="_blank">
+          Open Webplayer
+        </a>
+        <a href="https://jukebox.gg/partnership" class="btn btn-lg btn-outline-light" target="_blank">
+          Sign up as Artist
+        </a>
+      </div>
+    </div>
 </template>
 
 <script>
--- resources/js/views/Viewer.vue
+++ resources/js/views/Viewer.vue
@@ -1,74 +1,67 @@
 <template>
-    <div class="viewer">
-        <h2>Hello, World!</h2>
-        <p>Would you care to cycle a color?</p>
-        <div>
-            <input type="button" id="cycle" value="Yes, I would" @click="cycleColor" :disabled="disabled">
+  <div class="viewer p-3 overflow-hidden">
+    <div v-if="track">
+      <div class="media">
+        <img :src="track.album.image_url" class="mr-3 rounded" height="75" alt="cover">
+        <div class="media-body align-self-center">
+          <div class="font-weight-bold text-truncate">{{ track.name }}</div>
+          <small class="mb-0 text-white-50 text-truncate">{{ track.album.name }}</small>
         </div>
-        <div style="float: left; position: relative; left: 50%;">
-            <div id="color" :style="'border-radius: 50px; transition: background-color 0.5s ease; margin-top: 30px; width: 100px; height: 100px; background-color:' + color + '; float: left; position: relative; left: -50%'">
-            </div>
-        </div>
-        <div id="list">
-        </div>
+      </div>
     </div>
+    <div v-else>
+      <div class="media w-100">
+        <img src="https://cdn.jukebox.gg/default.jpg" class="mr-3 rounded" height="75" alt="cover">
+        <div class="media-body align-self-center">
+          <div class="font-weight-bold text-truncate">Empty Player Queue</div>
+          <small class="mb-0 text-white-50 text-truncate">Powered by Jukebox</small>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import Twitch from '../mixins/Twitch';
 
 export default {
-    name: 'Viewer',
+  name: 'Viewer',
 
-    mixins: [
-        Twitch
-    ],
+  mixins: [
+    Twitch
+  ],
 
-    data () {
-        return {
-            color: '#6441A4',
-            disabled: 'disabled'
-        }
-    },
-
-    mounted () {
-        this.twitch.listen('broadcast', (target, contentType, color) => {
-            logger('Received broadcast color ' + color);
-            this.updateBlock(color);
-        });
-    },
-
-    methods: {
-        boot () {
-            logger('Enabling...');
-            this.disabled = false;
-            this.getColor();
-
-            this.finishedLoading = true;
-        },
-
-        cycleColor () {
-            logger('Requesting a color cycle');
-
-            this.$http.post('https://localhost:8081/color/cycle')
-            .then(response => { this.updateBlock(response.data) })
-            .catch(error => { this.logError(error); });
-        },
-
-        getColor () {
-            this.$http.get('https://localhost:8081/color/query')
-            .then(response => { this.updateBlock(response.data) })
-            .catch(error => { this.logError(error); });
-        },
-
-        logError (error) {
-            logger('EBS request returned '+error.status+' ('+error+')');
-        },
-
-        updateBlock (hex) {
-            logger('Updating block color');
-            this.color = hex;
-        }
+  data() {
+    return {
+      track: null,
     }
+  },
+
+  mounted() {
+    this.updateTrack('106415581');
+  },
+
+  methods: {
+    boot () {
+      logger('Enabling...');
+
+      this.updateTrack(this.auth.getChannelId());
+
+      this.finishedLoading = true;
+    },
+
+    updateTrack(channelId) {
+      axios
+          .get(`https://api.jukebox.gg/v1/twitch-extensions/${channelId}/currently-playing`)
+          .then(response => {
+            if (response.status === 204) {
+              this.track = null;
+            } else {
+              this.track = response.data;
+            }
+          })
+          .catch(error => console.error(error));
+    }
+  }
 }
 </script>
